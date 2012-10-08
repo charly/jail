090e2c59b84a75dffcc96550bdf5f8e6035d574d
charlysisto <charlysisto@gmail.com>
2012-10-08 18:44:55 +0200

    basic test for github

--- spec/models/github_spec.rb
+++ spec/models/github_spec.rb
@@ -1,47 +1,71 @@
 require "spec_helper"
 
 # TODO : TAFT
-decribe Jail::Github do
+describe Jail::Github, :vcr do
   
   describe "#github" do
-    it "holds a Github instance" do
-      true
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
+    
+    it "holds a Github::Client (API v3) instance" do
+      git.github.should be_a(Github::Client)
+    end
+  end
+
+  describe "#where(path)" do
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
+
+    it "sets #path to the arg it's passed" do
+      git.where("/chosen")
+      git.path.should == "/chosen"
+    end
+
+    it "is chainable (returns self)" do
+      git.where("/").should == git
     end
   end
 
   describe "#repos" do
-    it "holds a Github::Repo instance" do
-      true
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
+    
+    it "holds a Github::Repo(API v3) response" do
+      git.repos.should be_a(Hashie::Mash)
+      git.repos.should respond_to(:owner, :watchers, :full_name, :language)
     end
-
   end
 
   describe "#contents" do
-    it "holds a Github::Content instance" do
-      true
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
+    
+    it "holds a Github::Content (API v3) response" do
+      content = git.contents.first
+      content.should be_a(Hashie::Mash)
+      content.should respond_to(:sha, :path, :name, :type)
     end
-
   end
 
-  describe "#install" do
-    it "calls download 3 times instance" do
-      true
-    end
+  describe "#readme" do
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
 
+    it "returns the readme decoded" do
+      git.readme.should match(/Chosen is a library for making/)
+    end
   end
 
-  describe "#download" do
-    it "gets content from the filename instance" do
-      true
-    end
+  describe "#read" do
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
 
+    it "decodes the base64 of the current content (if it's a file)" do
+      git.where("VERSION").read.should match(/\d+.\d+.\d+/)
+    end
   end
 
-  describe "#target" do
-    it "holds sets a Pathanme" do
-      true
-    end
+  describe "#target( type )" do
+    let(:git) { Jail::Github.find("harvesthq", "chosen") }
 
+    it "returns a Pathanme" do
+      git.where("chosen/chosen.css").target(:js).should == 
+        Rails.root.join("vendor/assets/javascripts/chosen.css")
+    end
   end
 
 end
