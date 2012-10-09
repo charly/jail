62134e1238f18874e2aba615d522b1d42e8ee9eb
charlysisto <charlysisto@gmail.com>
2012-10-09 22:56:17 +0200

    fun with dummy app

---
+++ spec/dummy/app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+# pages_controller.rb
+class PagesController < ApplicationController
+  def index
+  end
+end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
+  root :to => "pages#index"
   mount Jail::Engine => "/jail" if  !Rails.env.production?
+
 end
